df525471aa956c54e1a339c525e9e6dc1d073476
andreamsgi27 <andreamsgi27@hotmail.com>
2024-10-20 22:15:51 +0200

    "Added two new methods to JavalistApplication class: crearDias() and dias()"

--- javalist/src/main/java/org/factoriaf5/javalist/JavalistApplication.java
+++ javalist/src/main/java/org/factoriaf5/javalist/JavalistApplication.java
@@ -1,5 +1,7 @@
 package org.factoriaf5.javalist;
 
+import java.util.List;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,6 +10,14 @@ public class JavalistApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(JavalistApplication.class, args);
+	}
+
+	public List<String> crearDias() {
+		String[] dias = {"Lunes", "Martes", "Miércoles", "Jueves",  "Viernes", "Sábado", "Domingo"};
+		return dias;
+	}
+	public void dias(){
+		String[] dias = {"Lunes", "Martes", "Miércoles", "Jueves",  "Viernes", "Sábado", "Domingo"};
 	}
 
 }
