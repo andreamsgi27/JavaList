9c80bbd11245090b911ef99c37c54d4f2a7f2009
andreamsgi27 <andreamsgi27@hotmail.com>
2024-10-21 18:14:28 +0200

    "Added new methods to JavalistApplication class: crearDias, verDias, largoSemana, eliminaDia, dia, and diaExiste. Modified existing method signatures and implementations."

--- javalist/src/main/java/org/factoriaf5/javalist/JavalistApplication.java
+++ javalist/src/main/java/org/factoriaf5/javalist/JavalistApplication.java
@@ -1,5 +1,6 @@
 package org.factoriaf5.javalist;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.boot.SpringApplication;
@@ -10,14 +11,50 @@ public class JavalistApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(JavalistApplication.class, args);
+
 	}
 
-	public List<String> crearDias() {
-		String[] dias = {"Lunes", "Martes", "Miércoles", "Jueves",  "Viernes", "Sábado", "Domingo"};
+	public static void crearDias (){
+		List<String> dias = new ArrayList<>();
+		dias.add("Lunes");
+		dias.add("Martes");
+		dias.add("Miercoles");
+		dias.add("Jueves");
+		dias.add("Viernes");
+		dias.add("Sabado");
+		dias.add("Domingo");
+	}
+
+	public static List<String> verDias(){
+		List<String> dias = List.of("Lunes", "Martes", "Miércoles", "Jueves",  "Viernes", "Sábado", "Domingo");
 		return dias;
 	}
-	public void dias(){
-		String[] dias = {"Lunes", "Martes", "Miércoles", "Jueves",  "Viernes", "Sábado", "Domingo"};
+
+	public int largoSemana (List<String> verDias){
+		int largoSemana  = verDias.size();
+		return largoSemana;
 	}
+
+	public static void eliminaDia (List<String> verDias){
+		verDias.remove(0);
+	}
+
+	public String dia (List<String> verDias, int dia){
+		String diaSemana = verDias.get(dia);
+		return diaSemana;
+	}
+
+	public boolean diaExiste (List<String> verDias, int dia){
+		if (verDias.contains(dia)){
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	
+
+
+
 
 }
